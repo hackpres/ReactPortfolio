a8df6f4b00a85bb885ecef9d498eb49fbc9b5336
hackpres <102390515+hackpres@users.noreply.github.com>
2022-11-07 11:25:37 -0500

    updated bio text to align with brand statement

--- src/components/bio/bioData.js
+++ src/components/bio/bioData.js
@@ -3,7 +3,7 @@ import Alex from '../../assets/images/alexanderHacker.png';
 const bioData = {
     name: 'Alexander Hacker',
     image: `${Alex}`,
-    bio: 'Full Stack Web Developer bringing years of building and design experience to help clients create aesthetically pleasing and functionally smooth web applications.'
+    bio: 'Web developer with comprehensive experience handling various projects from creative design to project construction. Certificate from Georgia Tech Professional Education in full stack web technologies, including React, JavaScript, NoSQL, GraphQL, Node.js, CSS, and HTML. I am fascinated by coding logic and de-structuring challenges into logical step-by-step solutions. I strive to finish everyday having played a part in making the web a better place for everyone.'
 };
 
 
